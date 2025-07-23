4bbe7151ac7fa162d70438d236fb2a222340ac07
ALEYuzuki <73732006+ALEYuzuki@users.noreply.github.com>
2025-07-23 22:55:04 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,10 @@
-// used for rendering equations (optional)
-import 'katex/dist/katex.min.css'
-// used for code syntax highlighting (optional)
-import 'prismjs/themes/prism-coy.css'
-// core styles shared by all of react-notion-x (required)
-import 'react-notion-x/src/styles.css'
-// global styles shared across the entire site
-import 'styles/global.css'
-// this might be better for dark mode
-// import 'prismjs/themes/prism-okaidia.css'
-// global style overrides for notion
-import 'styles/notion.css'
-// global style overrides for prism theme (optional)
-import 'styles/prism-theme.css'
-
 import type { AppProps } from 'next/app'
 import * as Fathom from 'fathom-client'
 import { useRouter } from 'next/router'
 import { posthog } from 'posthog-js'
 import * as React from 'react'
 
-import Layout from '@/components/Layout' // ← Layout.tsx を読み込み
-
+import Layout from '@/components/Layout'
 import { bootstrap } from '@/lib/bootstrap-client'
 import {
   fathomConfig,
@@ -29,6 +13,21 @@ import {
   posthogConfig,
   posthogId
 } from '@/lib/config'
+
+// core styles shared by all of react-notion-x (required)
+import 'react-notion-x/src/styles.css'
+// used for rendering equations (optional)
+import 'katex/dist/katex.min.css'
+// used for code syntax highlighting (optional)
+import 'prismjs/themes/prism-coy.css'
+// this might be better for dark mode
+// import 'prismjs/themes/prism-okaidia.css'
+// global styles shared across the entire site
+import 'styles/global.css'
+// global style overrides for notion
+import 'styles/notion.css'
+// global style overrides for prism theme (optional)
+import 'styles/prism-theme.css'
 
 if (!isServer) {
   bootstrap()
@@ -63,7 +62,6 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
-  // ← Notionの中身をLayoutで包んで見た目を整える
   return (
     <Layout>
       <Component {...pageProps} />
