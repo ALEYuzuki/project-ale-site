5514f8f853a72e222620950d2fba6edd4ed51395
ALEYuzuki <73732006+ALEYuzuki@users.noreply.github.com>
2025-07-23 23:44:50 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,12 @@
-import Layout from '@/components/Layout'
 import { NotionPage } from '@/components/NotionPage'
-import { GetStaticProps } from 'next'
 import { getPage } from '@/lib/notion'
-import { ExtendedRecordMap } from 'notion-types'
 
-type Props = {
-  recordMap: ExtendedRecordMap
+export default function Home(props) {
+  return <NotionPage recordMap={props.recordMap} />
 }
 
-export default function Home({ recordMap }: Props) {
-  return (
-    <Layout>
-      <NotionPage recordMap={recordMap} />
-    </Layout>
-  )
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const recordMap = await getPage('2396949e86ee80bcaae4d10d9b2c6e67') // ← your rootNotionPageId
-
+export async function getStaticProps() {
+  const recordMap = await getPage('2396949e86ee80bcaae4d10d9b2c6e67')
   return {
     props: {
       recordMap,
